9efaca72406c8c5ea3f9bc7e0ebbcd4d346006d5
qian li <lee@123.com>
2021-03-11 14:58:28 +0800

    init gshop

---
+++ src/api/ajax.js
@@ -0,0 +1,32 @@
+/*
+ajax请求函数模块
+返回值： promise对象(异步返回的数据是：response.data)
+
+*/
+import axios from 'axios'
+
+export default function ajax(url, data={}, type='GET'){
+  return new Promise(function(resolve, reject){
+    //执行异步ajax请求
+    let promise
+    if (type==='GET'){
+      let dataStr = ''     //数字拼接字符串
+      Object.keys(data).forEach(key=>{
+        dataStr += key + '=' + data[key] + '&'
+      })
+      if (dataStr !== ''){
+        dataStr = dataStr.substring(0,dataStr.lastIndexOf('&'))
+        url = url + '?' + dataStr
+      }
+      promise = axios.get(url)        //发送get请求
+    }else{
+      promise = axios.post(url,data)  //发送post请求
+    }
+    promise.then(function (response){
+      resolve(response.data)   //成功了,调用resolve（）
+    }).catch(function(error){
+      reject(error)       //失败了,调用reject（）
+    })
+  })
+
+}
---
+++ src/components/HeaderTop/HeaderTop.vue
@@ -0,0 +1,61 @@
+<template>
+  <header class="header">
+    <slot name="left"></slot>
+    <span class="header_title">
+      <span class="header_title_text ellipsis">{{title}}</span>
+    </span>
+    <slot name="right"></slot>
+  </header>
+</template>
+
+<script>
+  //认为Msite.vue为该组件的父组件，slot是父组件向子组件传递标签数据，最后该组件作为子组件引入Msite.vue组件
+  //slot传递的是标签，props传递的是数据
+  export default {
+    props:{
+      title: String
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .header
+    background-color #02a774
+    position fixed
+    z-index 100
+    left 0
+    top 0
+    width 100%
+    height 45px
+    .header_search
+      position absolute
+      left 15px
+      top 50%
+      transform translateY(-50%)
+      width 10%
+      height 50%
+      .icon-sousuo
+        font-size 25px
+        color #fff
+    .header_title
+      position absolute
+      top 50%
+      left 50%
+      transform translate(-50%, -50%)
+      width 50%
+      color #fff
+      text-align center
+      .header_title_text
+        font-size 20px
+        color #fff
+        display block
+    .header_login
+      font-size 14px
+      color #fff
+      position absolute
+      right 15px
+      top 50%
+      transform translateY(-50%)
+      .header_login_text
+        color #fff
+</style>
